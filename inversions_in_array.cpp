0e226476cea3d340a0950e0a465e50d2f781e112
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-07-02 15:44:24 +0530

    Create inversions_in_array.cpp

---
+++ inversions_in_array.cpp
@@ -0,0 +1,12 @@
+//simple approach i understood and did
+int inversion(int arr[],int n){
+    int count=0;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j] && i<j){
+                count++;
+            }
+        }
+    }
+    return count;
+}
