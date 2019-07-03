eca9d51c576f575760e159c837806937fd70b4b6
Jyoti Ghanghoriya <ghanghoriya.jyoti@gmail.com>
2019-07-03 12:03:38 +0530

    Create mini_distance_btw_two_nos.cpp

---
+++ mini_distance_btw_two_nos.cpp
@@ -0,0 +1,8 @@
+NOte;
+minimum distance between two points , see here pt to not is that there can be repeatition of element so we need to always
+pick the elements which are having minimum distance amaong them
+okk there exist some problem although its a basic level question i m not able to implement it
+
+below is the link please try again later and u know the logic just lil bit of thinking and u will do it.
+
+https://practice.geeksforgeeks.org/problems/minimum-distance-between-two-numbers/1
